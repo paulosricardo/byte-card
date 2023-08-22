91620cccef1c52fe93f6f15156f3a10f38a57f1f
paulosricardo <paulosoliveia2011@gmail.com>
2023-08-21 21:08:29 -0300

    finaliza tarefas da semana 2

--- teste_cartao.py
+++ teste_cartao.py
@@ -1,11 +1,7 @@
 #Projeto ByteCard 
 
-from Cartao import Cartao
+from model import Cartao, CompraCredito, Compra
 
-#visa = Cartao()
-#mastercard = Cartao()
-#print(visa)
-#print(mastercard)
  
 visa = Cartao('4321 8765 9807 2222', '07/2032', '001', 4000.0, 'Ricardo Paulos')
 
@@ -41,5 +37,50 @@ print(visa.limite)
 print(mastercard.limite)
 
 
+compra_amazon = CompraCredito(1000.0, '15/02/2023 19:46:17', 'Amazon', 'Casa',visa, 10)
+
+
+compra_farmacia_visa = Compra(200.0, '01/01/2023 10:00:00', 'Farmácia Drosil','Saude',visa)
+compra_restaurante_visa = Compra(89.9, '02/01/2023 12:15:00', 'Burguer King', 'Lazer',visa)
+compra_supermercado_visa = Compra(475.5, '03/02/2023 07:05:05', 'Extra','Alimentação',visa)
+
+
+compra_farmacia_mastercard = Compra(400.0, '06/01/2023 12:30:00', 'Farmácia São Paulo','Saúde',mastercard)
+compra_restaurante_mastercard = Compra(89.9, '07/07/2023 15:05:00', 'Burguer mcdonald', 'Lazer',mastercard)
+compra_supermercado_mastercard = Compra(475.5, '08/09/2023 07:05:05', 'Atacadão','Alimentação',mastercard)
+
+
+
+
+print(compra_farmacia_visa)
+print(compra_farmacia_mastercard)
+
+print(compra_restaurante_visa)
+print(compra_restaurante_mastercard)
+
+print(compra_supermercado_visa)
+print(compra_supermercado_mastercard)
+
+
+
+
+print(f'Compra a crédito de visa: {compra_amazon.valor} em {compra_amazon.quantidade_parcelas} x de {compra_amazon.valor_parcela}')
+
+fatura = [compra_farmacia_visa, compra_restaurante_visa, compra_supermercado_visa, compra_amazon]
+total = 0
+for compra in fatura:
+	total += compra.valor
+
+print(f'O total da fatura é: {total}')
+  
+print(f'Compra a crédito de mastercard: {compra_amazon.valor} em {compra_amazon.quantidade_parcelas} x de {compra_amazon.valor_parcela}')
+
+fatura = [compra_farmacia_mastercard, compra_restaurante_mastercard, compra_supermercado_mastercard, compra_amazon]
+total = 0
+for compra in fatura:
+	total += compra.valor
+
+print(f'O total da fatura é: {total}')
+  
 
 
